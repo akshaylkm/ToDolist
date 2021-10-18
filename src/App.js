c514fb747b34903e3f79774f72e27d2781e61726
Akshay KM <akshaykm194@gmail.com>
2021-10-18 23:09:59 +0530

    final file

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+import'./app.css'
+function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo]=useState('')
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(), text: toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+        { toDos.map((obj)=>{
+
+        return (<div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.value);
+              console.log(obj);
+            }} value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>)
+         }) }
+      </div>
+    </div>
+  );
+}
+
+export default App;
